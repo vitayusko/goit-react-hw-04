4554b2eda4f9d9260e1bb5ba573c8374235741ea
vitayusko <vitayusko@users.noreply.github.com>
2024-07-25 15:01:58 +0200

    подгрузка фото

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,30 +3,46 @@ import SearchBar from "./SearchBar/SearchBar";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import { fetchImages } from "../services/api";
 import "../index.css";
+import Loader from "./Loader/Loader";
+import { ErrorMessage } from "formik";
+import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
 
 // Импорт функции fetchImages взаимодействие с бэк эндом
 
 const App = () => {
   const [photo, setPhoto] = useState([]);
   const [query, setQuery] = useState("nature"); //для обработки запроса в серч баре
+  const [isLoading, setIsLoading] = useState(false); //ШФГ5 загрузка
+  const [isError, setIsError] = useState(false); //ШФГ6 ошибки
+  const [page, setPage] = useState(1); // Шаг 7 добавляем подгрузку картинок
 
   //шаг 3 взаимодействие с бэк эндом, рендер фоток в галерею
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await fetchImages(query); // Заменить "nature" на любой другой запрос
-        setPhoto(response.results); // Измените на response.results, если API Unsplash возвращает данные в таком формате
+        setIsLoading(true); //идет загрузка
+        setIsError(false);
+        const response = await fetchImages(query, 9, page); // Заменить "nature" на любой другой запрос
+        setIsLoading(false); //отменяем загрузку после того как вывели изображение
+
+        setPhoto((prev) => [...prev, ...response.results]); // Измените на response.results, если API Unsplash возвращает данные в таком формате
       } catch (error) {
         console.log(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
       }
     };
     getData();
-  }, [query]); //  пустой массив зависимостей, чтобы useEffect выполнялся один раз
+  }, [query, page]); //  пустой массив зависимостей, чтобы useEffect выполнялся один раз
 
   return (
     <div>
       <SearchBar setQuery={setQuery} />
       <ImageGallery items={photo} />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      <LoadMoreBtn onClick={() => setPage((prev) => prev + 1)} />
     </div>
   );
 };
---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./ErrorMessage/ErrorMessage.module.css";
+
+const ErrorMessage = () => {
+  return (
+    <div className={s.wrapper}>
+      <h1>ERROR!</h1>
+    </div>
+  );
+};
+
+export default ErrorMessage;
